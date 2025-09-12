afd914d83db033d41b12b7e750bf97f33a490ce9
BURIBURI-ZAEMON1 <3511457481@qq.com>
2025-09-12 22:08:17 +0800

    Delete Back_End directory

--- Back_End/app/model/user.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type User struct {
-	ID       int   `json:"id"`
-	Username string `json:"username"`
-	Name     string `json:"name"`
-	Password string `json:"-"`
-	UserType int    `json:"usertype"`
-}
